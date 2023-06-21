4e9afc7e6cf1b0bd618fa49cf882916652203609
Céline Gourc <celine.gourc1@laposte.net>
2023-06-21 14:08:31 +0200

    variables env

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const nodemailer = require("nodemailer");
-const dotenv = require('dotenv').config();
-const mg = require('nodemailer-mailgun-transport');
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const nodemailer = require("nodemailer")
+const dotenv = require('dotenv').config()
+const mg = require('nodemailer-mailgun-transport')
 
 const PORT = process.env.PORT || 8000;
 app.use(cors())
